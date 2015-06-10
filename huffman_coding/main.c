6e6e1efbe337ab4e2a84ccaad1d457f0387ceda5
Victor Cavalcante <victor.cavalcantexxii@gmail.com>
2015-06-10 12:06:30 -0300

    (partial) Opening file and translating each char into its binary equivalent.

--- huffman_coding/main.c
+++ huffman_coding/main.c
@@ -20,7 +20,7 @@ int main(){
 	char binaryPath[MAX_BIN_PATH] = "";
 	char pathDirection[5] = "";
 
-// STEP ONE - Open File > Save text > Genereate Queue > Generate Tree ====================
+// STEP ONE - Open File > Save text > Genereate Queue > Generate Tree > Create Binary Dictionary ======
 	// Open File
 	pFile = fopen("test.txt", "r");
 	if(pFile == NULL){
@@ -33,7 +33,7 @@ int main(){
 		strcat(file_text, strAux);
 	}
 	removeLineBreakOfString(file_text);
-
+	fclose(pFile);
 	// Generating Ocurrence priority queue
 	occurrenceQueue = generateCharacterOcurrenceQueue(occurrenceQueue, file_text);
 
@@ -43,11 +43,49 @@ int main(){
 	// Generating hash with binary dictionary
 	Hashtable* dictionary = createHashTable();
 	create_binary_dictionary_hashtable(huffmanTree, binaryPath, pathDirection, dictionary);
-	printHashTable(dictionary);
+
+// STEP TWO - ====================
+	/*FILE *outFile;
+	char unChar[8] = "This is";
+
+	outFile = fopen("outTest.huff", "w+b");
+	if(outFile == NULL){
+		printf("Error writing file");
+		exit(0);
+	}
+	fwrite(unChar, sizeof(unChar), 1, outFile);
+	fclose(outFile);*/
+
+	FILE *tFile = fopen("test.txt", "r");
+	Queue *binaryTrans = createQueue();
+	char currChar, currBin[100];
+	int i;
+	if(tFile == NULL){
+		printf("Error opening file");
+		exit(0);
+	}
+	while((currChar = fgetc(tFile)) != EOF) {
+		strcpy(currBin, get(dictionary, currChar));
+		for(i = 0; i < strlen(currBin); i++){
+			enqueue(binaryTrans, currBin[i]);
+		}
+		if(getQueueLength(binaryTrans) >= 8){
+			//todo dequeue 8 times while creating binary (setBit foo), then write it on the new .huff file
+			for(i = 0; i < 8; i++){
+				printf("%c", dequeueAndFree(binaryTrans));
+			}
+			printf("\n");
+		}
+	}
+	if(strlen(currBin) > 0){
+		//todo get trash size here
+	}
+	freePriorityQueue(binaryTrans);
+	fclose(tFile);
 
 	//Free & close
 	freePriorityQueue(occurrenceQueue);
 	freeHashTable(dictionary);
-	fclose(pFile);
+
 	return 0;
 }
