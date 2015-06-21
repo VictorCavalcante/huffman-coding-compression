5275595ddb1d3063b05e9347aac4c80951126dbd
Victor Cavalcante <victor.cavalcantexxii@gmail.com>
2015-06-21 14:40:45 -0300

    Adapting & realocating functions from 'auxiliary' to 'hashtable'. Adding custom 'put_occurrence' function.

--- huffman_coding/hashtable_list.c
+++ huffman_coding/hashtable_list.c
@@ -5,11 +5,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "priority_queue.h"
+#define MAX_BIN_PATH 100
 #define H_MAX 13
 
 typedef struct h_node{
 	char binary[9];
 	char keyLetter;
+	int occurrence;
 	struct h_node* nextElement;
 }H_node;
 
@@ -28,6 +31,27 @@ Hashtable* createHashTable(){
 
 int hashGenerator(char key){
 	return key % H_MAX;
+}
+
+void put_occurrence(Hashtable* ht, char keyLetter){
+	int h = hashGenerator(keyLetter);
+	H_node *current = ht->table[h];
+	//Search for update
+	while(current != NULL){
+		if(current->keyLetter == keyLetter){ //Update
+			current->occurrence++;
+			break;
+		 }
+		current = current->nextElement;
+	}
+	//Insert front
+	if(current == NULL){
+		H_node* newElement = (H_node*)malloc(sizeof(H_node));
+		newElement->keyLetter = keyLetter;
+		newElement->occurrence = 1;
+		newElement->nextElement = ht->table[h];
+		ht->table[h] = newElement;
+	}
 }
 
 void put(Hashtable* ht, char *binary, char keyLetter){
@@ -76,4 +100,34 @@ void freeHashTable(Hashtable *ht){
 			free(previous);
 		}
     }
+}
+
+void create_binary_dictionary_hashtable(Q_node* node, char *path, char *direction, Hashtable* ht){
+	char pathTurn[MAX_BIN_PATH];
+	if(strcmp(direction, "left") == 0){
+			strcat(path, "0");
+	} else if (strcmp(direction, "right") == 0){
+		strcat(path, "1");
+	}
+	strcpy(pathTurn, path);
+
+	if(getQueueNodeValue(node) == '*'){
+		create_binary_dictionary_hashtable(getQueueNodeLeft(node), path, "left", ht);
+		create_binary_dictionary_hashtable(getQueueNodeRight(node), pathTurn, "right", ht);
+	} else {
+		put(ht, path, getQueueNodeValue(node));
+	}
+}
+
+Queue* generateCharacterOcurrenceQueue(Queue* queue, Hashtable *occurrenceHash){
+	int i;
+	H_node *current;
+	for(i = 0; i < H_MAX; i++){
+		current = occurrenceHash->table[i];
+		while(current != NULL){
+			queue = PQ_enqueue(queue, current->keyLetter, current->occurrence);
+			current = current->nextElement;
+		}
+	}
+	return queue;
 }
--- huffman_coding/hashtable_list.h
+++ huffman_coding/hashtable_list.h
@@ -14,6 +14,8 @@ Hashtable* createHashTable();
 
 int hashGenerator(char key);
 
+void put_occurrence(Hashtable* ht, char keyLetter);
+
 void put(Hashtable* ht, char *binary, char keyLetter);
 
 char* get(Hashtable* ht, char keyLetter);
@@ -21,5 +23,23 @@ char* get(Hashtable* ht, char keyLetter);
 void printHashTable(Hashtable *ht);
 
 void freeHashTable(Hashtable *ht);
+
+/**
+ * Receives a tree as first parameter then iterates the tree in pre-order
+ * concatenating 0 everytime it moves the pointer to the left and 1 when it moves the pointer
+ * to the right, when the pointer reaches the traversal '*' it then pushes the string containing
+ * the 0's and 1's and node value into a hashtable, generating a dictionary of characters and
+ * correspondent binary representation.
+ */
+void create_binary_dictionary_hashtable(Q_node* node, char *path, char *direction, Hashtable* ht);
+
+/**
+ * Sorts the given string, then calculates the frequency of ocurrence of
+ * each different character on given string. Later, pushes each different
+ * character into a priority queue using its frequency as priority.
+ * Returns the newly created queue containing every character
+ * and its frequency of occurence on each node.
+ */
+Queue* generateCharacterOcurrenceQueue(Queue* queue, Hashtable *occurrenceHash);
 
 #endif /* HASHTABLE_LIST_H_ */
